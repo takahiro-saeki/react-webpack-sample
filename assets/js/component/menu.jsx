73cdfbd455299b021ddc8d0679af1000bd36fea3
takahiro-saeki <hiroooon69@gmail.com>
2016-05-11 02:38:21 +0900

    add.

--- assets/js/component/menu.jsx
+++ assets/js/component/menu.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
 
 const Menu = () => (
   <AppBar
     title="Title"
-    iconClassNameRight="muidocs-icon-navigation-expand-more"
+    iconElementLeft={<IconButton><NavigationClose /></IconButton>}
+    iconElementRight={
+      <IconMenu
+        iconButtonElement={
+          <IconButton><MoreVertIcon /></IconButton>
+        }
+        targetOrigin={{horizontal: 'right', vertical: 'top'}}
+        anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+      >
+      </IconMenu>
+    }
   />
 );
 
