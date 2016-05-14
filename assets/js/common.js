0748c6c0fb7176477a178537650f2e5c1b3d457c
takahiro-saeki <hiroooon69@gmail.com>
2016-05-14 14:44:13 +0900

    the middle of commit.

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,20 +1,65 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import Menu from './component/menu';
-import Grid from './component/grid';
-import Footer from './component/footer';
+import {Dispatcher} from 'flux';
 
-const App = () => (
-  <MuiThemeProvider muiTheme={getMuiTheme()}>
-  <section>
-    <Menu />
-    <Grid />
-    <Footer />
-  </section>
-  </MuiThemeProvider>
-);
+class App extends React.Component {
+  render() {
+    return (
+      <div>test</div>
+    )
+  }
+}
+
+var flightDispatcher = new Dispatcher();
+var CountryStore = {country: null};
+var CityStore = {city: null};
+var FlightPriceStore = {price: null};
+
+flightDispatcher.dispatch({
+  actionType: 'city-update',
+  selectedCity: 'paris'
+})
+
+
+flightDispatcher.register(payload => {
+  if (payload.actionType === 'city-update') {
+    CityStore.city = payload.selectedCity;
+  }
+});
+
+flightDispatcher.dispatch({
+  actionType: 'country-update',
+  selectedCountry: 'australia'
+});
+
+
+CountryStore.dispatchToken = flightDispatcher.register(payload => {
+  if (payload.actionType === 'country-update') {
+    CountryStore.country = payload.selectedCountry;
+  }
+});
+
+CityStore.dispatchToken = flightDispatcher.register(payload => {
+  if (payload.actionType === 'country-update') {
+    flightDispatcher.waitFor([CountryStore.dispatchToken]);
+    CityStore.city = getDefaultCityForCountry(CountryStore.country);
+  }
+});
+
+FlightPriceStore.dispatchToken =
+  flightDispatcher.register(payload => {
+    console.log(payload)
+    switch (payload.actionType) {
+      case 'country-update':
+      case 'city-update':
+        flightDispatcher.waitFor([CityStore.dispatchToken]);
+        FlightPriceStore.price =
+          getFlightPriceStore(CountryStore.country, CityStore.city);
+        break;
+  }
+});
+
+
 
 ReactDOM.render(
   <App />,
